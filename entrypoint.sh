f84ca874e31768f594bae38d117bd7e5a48071e2
Anton Marin <antonaryo85@gmail.com>
2019-11-17 00:07:03 +0300

    mimic github pages jekyll plugins

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,15 @@ fi
 if [ ! -f Gemfile ]; then
     cat << EOF > Gemfile
 source 'https://rubygems.org'
-gem 'github-pages', group: :jekyll_plugins
+group :jekyll_plugins do
+    gem 'github-pages'
+    gem 'jekyll-paginate'
+    gem 'jekyll-sitemap'
+    gem 'jekyll-gist'
+    gem 'jekyll-feed'
+    gem 'jemoji'
+    gem 'jekyll-include-cache'
+end
 EOF
 fi
 bundle install
